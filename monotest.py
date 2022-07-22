aea355fef28a7f25f15668453da4b7179a622fd5
Rajesh Hugar <hugarrajesh@gmail.com>
2022-07-22 21:46:09 +0530

    this is my first code push in git with changes

--- monotest.py
+++ monotest.py
@@ -11,6 +11,70 @@ d= {
 
 }
 
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
 db1=client['mongotest']
 coll=db1['test']
 coll.insert_one(d)
