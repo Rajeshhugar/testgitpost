0c492140f08dac6b8a4240236556ed5df496010e
Rajesh Hugar <hugarrajesh@gmail.com>
2022-07-22 21:41:23 +0530

    this is my first code push in git

---
+++ monotest.py
@@ -0,0 +1,16 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://hugar_rajesh:Rajesh_0808@rajesh.ud2bw.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d= {
+    "name":"Rajesh Hugar",
+    "email_id":"rajesh_hugar@yahoo.in",
+    "surname":"hugar",
+    "Job_descrition":"Machine Learning Engineer"
+
+}
+
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
